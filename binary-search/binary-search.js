9e25c2ebbadf175ae584e1ff143780fcfc032a1f
Dana Zhang <danadaners@gmail.com>
2020-10-28 15:31:54 -0400

    working on number 1 recursive func

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,42 @@
 'use strict';
 
 // Complete this algo
+/*
+TODOS: * binarySearch is a function that takes an array and a target value. If the target is in the array, return true. Otherwise, return false.
+
+! * EDGE CASES:
+! * What if empty array?
+! * What if we pass in not an array into the first param?
+! * What if multi-dimensional array?
+! * What if we pass in a value that isn't a number(like if you pass in a target that is "6" but looking for 6) or target value is an array?
+*/
+
+
 const binarySearch = (array, target) => {
-	
+	//if empty array, return false
+	if(!array.length) return false;
+
+	//what if multidim array
+
+	//else loop through the array
+	for(let i = 0; i < array.length; i++){
+		if(array[i] === target){
+			return true;
+		}
+	}
+
+	return false;
+
 };
+
+// const binarySearch = (array, target) => {
+// 	let targetArray = array[target]
+// 	if(targetArray === undefined){
+// 		return false
+// 	}
+// 	return true
+// };
+
 
 /*
 	EXTRA CREDIT:
